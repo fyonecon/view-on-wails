d71ff110e400f1eccddc89eb55351bdfe89a92de
fyonecon <fyonecon@gmail.com>
2023-11-29 20:47:33 +0800

    调整Mac下的状态栏

--- main.go
+++ main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"embed"
+	"github.com/wailsapp/wails/v2/pkg/options/mac"
+	"github.com/wailsapp/wails/v2/pkg/options/windows"
 
 	"github.com/wailsapp/wails/v2"
 	"github.com/wailsapp/wails/v2/pkg/options"
@@ -17,16 +19,66 @@ func main() {
 
 	// Create application with options
 	err := wails.Run(&options.App{
-		Title:  "PureHome",
-		Width:  1024,
-		Height: 768,
+		Title:             "PureHome",
+		Width:             980,
+		Height:            640,
+		MinWidth:          320,
+		MinHeight:         480,
+		HideWindowOnClose: true,
+		Frameless:         false,
+		WindowStartState:  options.Normal,
 		AssetServer: &assetserver.Options{
 			Assets: assets,
 		},
-		BackgroundColour: &options.RGBA{R: 27, G: 38, B: 54, A: 1},
+		BackgroundColour: &options.RGBA{R: 51, G: 51, B: 51, A: 220}, // A最大255
 		OnStartup:        app.startup,
+		CSSDragProperty:  "--wails-draggable",
+		CSSDragValue:     "drag",
 		Bind: []interface{}{
 			app,
+		},
+		Mac: &mac.Options{
+			TitleBar: &mac.TitleBar{
+				TitlebarAppearsTransparent: true,
+				HideTitle:                  false, //
+				HideTitleBar:               false, // 三个点
+				FullSizeContent:            false,
+				UseToolbar:                 true,
+				HideToolbarSeparator:       true,
+			},
+			Appearance:           mac.NSAppearanceNameDarkAqua,
+			WebviewIsTransparent: true,
+			WindowIsTranslucent:  false,
+			About: &mac.AboutInfo{
+				Title:   "简单主页",
+				Message: "© 2023",
+				//Icon:    icon,
+			},
+		},
+		Windows: &windows.Options{
+			WebviewIsTransparent:              false,
+			WindowIsTranslucent:               false,
+			BackdropType:                      windows.Mica,
+			DisableWindowIcon:                 false,
+			DisableFramelessWindowDecorations: false,
+			WebviewUserDataPath:               "",
+			WebviewBrowserPath:                "",
+			Theme:                             windows.SystemDefault,
+			CustomTheme: &windows.ThemeSettings{
+				DarkModeTitleBar:   windows.RGB(20, 20, 20),
+				DarkModeTitleText:  windows.RGB(200, 200, 200),
+				DarkModeBorder:     windows.RGB(20, 0, 20),
+				LightModeTitleBar:  windows.RGB(200, 200, 200),
+				LightModeTitleText: windows.RGB(20, 20, 20),
+				LightModeBorder:    windows.RGB(200, 200, 200),
+			},
+			// User messages that can be customised
+			//Messages *windows.Messages
+			// OnSuspend is called when Windows enters low power mode
+			//OnSuspend func()
+			// OnResume is called when Windows resumes from low power mode
+			//OnResume func(),
+			//WebviewGpuDisabled: false,
 		},
 	})
 
